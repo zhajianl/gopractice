58d812c68bbfd182ecb86e1cbb2d80e124f55d2f
zhajianl <jianlinsky_009@163.com>
2022-01-16 16:54:51 +0800

    Update http_server.go

--- http_server.go
+++ http_server.go
@@ -1,30 +1,49 @@
 package main
-
 import (
-	"io"
-	"log"
-	"net/http"
-	"strings"
+        "fmt"
+        "log"
+        //"net"
+        "net/http"
+        //"net/http/pprof"
+        "os"
+        "strings"
 )
+func index(w http.ResponseWriter, r *http.Request) {
+        version := os.Getenv("VERSION")
+        w.Header().Set("VERSION", version)
+        fmt.Printf("os version: %s \n", version)
+        for k, v := range r.Header {
+                for _, vv := range v {
+                        fmt.Printf("Header key: %s, Header value: %s \n", k, v)
+                        w.Header().Set(k, vv)
+                }
+        }
+        clientip := getCurrentIP(r)
+        log.Printf("Success! Response code: %d", 200)
+        log.Printf("Success! clientip: %s", clientip)
+}
 
 func healthz(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "200")
-	log.Println("/healthz Client IP Address is " + r.RemoteAddr + " http Status Code is " + http.StatusText(200))
+
+        fmt.Fprintf(w, "200")
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	for key, value := range r.Header {
-		w.Header().Add(key, strings.Join(value, " "))
-	}
-	log.Println("/ Client IP Address is " + r.RemoteAddr + " http Status Code is " + http.StatusText(200))
+
+func getCurrentIP( r *http.Request) string {
+        ip := r.Header.Get("X-Real-IP")
+        if ip == "" {
+          ip = strings.Split(r.RemoteAddr,":")[0]
+        }
+        return ip
+
 }
+
 
 func main() {
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/healthz", healthz)
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+        mux := http.NewServeMux()
+        mux.HandleFunc("/", index)
+        mux.HandleFunc("/healthz", healthz)
+        if err := http.ListenAndServe(":8080", mux); err != nil {
+                log.Fatalf("start http server failed, error: %s\n", err.Error())
+        }
 }
